e28756f07cd46f818df3da7c004883bd623b2eef
Tim Banta <timbanta@me.com>
2022-10-10 15:36:58 -0700

    feat: add reducer function and pass to createStore()

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function createStore() {
+// Library Code
+function createStore(reducer) {
   // The store should have four parts:
   // 1. The state
   // 2. Get the state
@@ -6,8 +7,34 @@ function createStore() {
   // 4. Update the state
 
   let state
+  let listeners = []
 
   const getState = () => state
 
-  return { getState }
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter((l) => l !== listener)
+    }
+  }
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  }
+}
+
+// App Code
+function todo(state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  return state
 }
